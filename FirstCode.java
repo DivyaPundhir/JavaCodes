9fe2245fc7dc4c1b3015c66b18ac11cd3877c623
ZeroToPro <96923915+DivyaPundhir@users.noreply.github.com>
2022-03-21 23:18:12 +0530

    March 21, 2022 - First Java Code
    I will not stop now.

---
+++ FirstCode.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class FirstCode here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FirstCode
+{
+    public static void main(String[] args){
+
+        System.out.println("Everything happens for a reason");
+        System.out.println("It's okay if people don't trust in you");
+        System.out.println("I will not answer them with my words");
+        System.out.println("I know my worth");
+        System.out.println("I will never Give Up");
+
+    }
+}
